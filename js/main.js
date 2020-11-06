a4aa8bb7bf8d9afafe72ba325fd33be683a8890b
Jonahmallard <jonahmallard@outlook.com>
2020-11-06 16:02:55 -0600

    Add render function

--- js/main.js
+++ js/main.js
@@ -42,3 +42,29 @@ function getWinner(){
     }
 }
 
+function render() {
+    holes.forEach(function(numStones, idx){
+        let holeEl = document.getElementById('holes' + idx);
+        holeEl.innerHTML = numStones;
+    });
+
+    p1ScoreEl.innerText = holes[6]; 
+    p2ScoreEl.innerText = holes[13]; 
+
+    if (gameWinner) {
+        document.querySelectorAll('h3').forEach(function(winner){
+            winner.innerHTML = ' Player ' + (gameWinner === 'one' ? 'One' : 'Two') + ' Won ! ';  
+        });
+        oneEl.style.border = twoEl.style.border = '';
+    } else {
+        player1.innerText = 'Player One ';
+        player2.innerText = 'Player Two ';
+        oneEl.style.border = currentPlayer === 'one' ? '2px solid white' : '';
+        twoEl.style.border = currentPlayer === 'two' ? '2px solid white' : '';
+    }
+};
+
+init();
+render();
+
+
